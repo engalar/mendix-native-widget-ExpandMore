8a65c869e3e0f25f1ed1522afe6dd381fb4ff17b
engalar <engalar@gmail.com>
2024-02-09 19:05:29 +0800

    first commit

---
+++ src/ExpandMore.jsx
@@ -0,0 +1,13 @@
+import { createElement, useCallback } from "react";
+
+import { Badge } from "./components/Badge";
+
+export function ExpandMore({ value, style, onClick }) {
+    const onClickHandler = useCallback(() => {
+        if (onClick && onClick.canExecute && !onClick.isExecuting) {
+            onClick.execute();
+        }
+    }, [onClick]);
+
+    return <Badge style={style} onClick={onClickHandler} value={value?.displayValue || "Default"} />;
+}
